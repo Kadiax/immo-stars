1cac4ee09a130c205c1c9c9f88f7f831d3d8fa20
Kadia Toure <kadiditoure@gmail.com>
2020-05-01 10:35:35 +0200

    add spring boot backend

---
+++ src/main/java/com/student/immostars/ImmoStarsApplication.java
@@ -0,0 +1,13 @@
+package com.student.immostars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoStarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmoStarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/student/immostars/ImmoStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.immostars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoStarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
